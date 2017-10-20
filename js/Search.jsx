466ba470f905954e47f87ddfdbb4ed305755a153
Jay Hongvarivatana <jayyhong@gmail.com>
2017-10-20 14:51:01 -0700

    Impletmented routes

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import ShowCard from "./ShowCard";
 import preload from "../data.json";
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: ""
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard key={show.imdbID} show={show} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
